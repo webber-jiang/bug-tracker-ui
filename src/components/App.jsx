4ba7816ab0fea100602252d50706719ba58fd170
webber-jiang <webbersjiang@gmail.com>
2020-04-14 13:36:47 +1200

    Test eslint

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+const hello = "123";
+
+function add(a, b) {
+  return a + b;
+}
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <div>{hello}</div>
+      <div>{add(2, 4)}</div>
+    </>
+  );
 };
 
 export default App;
