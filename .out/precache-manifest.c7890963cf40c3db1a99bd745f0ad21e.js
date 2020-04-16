0a60bfb0c323884de25bd9ad06e76cdfe2d9ab3a
webber-jiang <webbersjiang@gmail.com>
2020-04-16 12:50:31 +1200

    .out file for Remote build

---
+++ .out/precache-manifest.c7890963cf40c3db1a99bd745f0ad21e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f0d8c7abd18df6b159e5470ac7662e2",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.7b4444bc5aa09b4947f2.bundle.js"
+  },
+  {
+    "url": "runtime~main.7b4444bc5aa09b4947f2.bundle.js"
+  },
+  {
+    "url": "vendors~main.7b4444bc5aa09b4947f2.bundle.js"
+  },
+  {
+    "url": "vendors~main.7b4444bc5aa09b4947f2.bundle.js.LICENSE.txt"
+  }
+]);
