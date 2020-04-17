a49bf74d7d888e6dc4c89da0835eaf1a13d85d5e
webber-jiang <webbersjiang@gmail.com>
2020-04-17 16:48:40 +1200

    pre-commit

---
+++ .out/precache-manifest.4f243f8ea09ca14b7679caab35cc84f4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecd942b863256f001124f35869311a24",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.2ad3a74f7a07990c9d98.bundle.js"
+  },
+  {
+    "url": "runtime~main.2ad3a74f7a07990c9d98.bundle.js"
+  },
+  {
+    "url": "vendors~main.2ad3a74f7a07990c9d98.bundle.js"
+  },
+  {
+    "url": "vendors~main.2ad3a74f7a07990c9d98.bundle.js.LICENSE.txt"
+  }
+]);
