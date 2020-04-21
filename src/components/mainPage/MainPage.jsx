8ad2234dfdeec4fb83916feec90ce25a08fc4450
webber-jiang <webbersjiang@gmail.com>
2020-04-21 17:22:18 +1200

    Setup auth0 and call API

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import api from "../../api";
 import { Link } from "react-router-dom";
 
 const MainPage = () => {
+  const callApi = async () => {
+    return await api.get("/projects");
+  };
+
+  callApi();
   return (
     <div>
       MainPage
