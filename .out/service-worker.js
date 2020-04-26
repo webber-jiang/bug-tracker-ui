c7e9c294145ae0913f8720df3a166f97db69db81
webber-jiang <webbersjiang@gmail.com>
2020-04-26 21:35:42 +1200

    Setup react testing library with demo

--- .out/service-worker.js
+++ .out/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.4f7d2ad78e42bd7b707d6b7a658befb7.js"
+  "precache-manifest.4bcc534c534b4377d1c7092f6c66d989.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
