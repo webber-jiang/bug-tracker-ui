1531b4ad4d26e79a93d3ade4bf6fe66d38054785
Ray2126 <raymondyang@outlook.co.nz>
2020-07-21 15:51:00 +1200

    role reducer

---
+++ src/reducers/RolesReducer.ts
@@ -0,0 +1,22 @@
+import _ from "lodash";
+
+import { FETCH_ROLES, FETCH_ROLE_BY_ID } from "../utils/Types";
+import Action from "./Action";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface RolesInitialState {}
+export const rolesInitialState: RolesInitialState = {};
+
+export const RolesReducer = (
+  state: RolesInitialState = rolesInitialState,
+  action: Action
+): object => {
+  switch (action.type) {
+    case FETCH_ROLE_BY_ID:
+      return { ...state, [action.payload.roleId]: action.payload };
+    case FETCH_ROLES:
+      return { ...state, ..._.mapKeys(action.payload, "roleId") };
+    default:
+      return state;
+  }
+};
